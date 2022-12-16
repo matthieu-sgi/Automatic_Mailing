a036e7a179af4994e3ab0324320113821ef2e7cb
sn00wden <matthieu2603@gmail.com>
2022-12-16 18:39:32 +0100

    Fixed gmail reject

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 """Send emails to a csv file of recipients with an attachment and a "body.html" template"""
-from __future__ import annotations
 
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
@@ -9,10 +8,12 @@ from os import getenv
 
 import smtplib
 import ssl
-import pandas as pd
+import time
 
-from tqdm import tqdm
 from dotenv import load_dotenv
+import pandas as pd
+from tqdm import tqdm
+from unidecode import unidecode
 
 
 # Add your .env file path
@@ -26,18 +27,31 @@ SERVER_NAME = "smtp.gmail.com"
 SERVER_PORT = 465
 
 # Pandas column names
-CIVILITE = "civilite"
-NOM = "nom"
-PRENOM = "prenom"
-MAIL = "mail"
+CIVILITE = "Salutation"#"civilite" # Mr, Mme, Mlle  #Prenom,Nom,Mail
+NOM = "Nom" # Last name
+PRENOM = "Prenom" # First name
+MAIL = "Mail" # Email address
 
 def write_body(html_path : str, civilite : str, nom: str, prenom : str) -> str:
     """Write the body of the email with the html template and the data from the csv file"""
     with open(html_path, "r",encoding="utf-8") as f:
         body = f.read()
-        body = body.replace(r"{civilite}",civilite)
-        body = body.replace(r"{nom}",nom)
-        body = body.replace(r"{prenom}",prenom)
+        # print(body.encode("utf-8"))
+        if str(civilite) != "nan":
+            body = body.replace(r"{civilite}", " " + str(civilite).replace(" ",""))
+        else:
+            body = body.replace(r"{civilite}","")
+
+        if str(nom) != "nan":
+            body = body.replace(r"{nom}", " " + str(nom).replace(" ",""))
+        else:
+            body = body.replace(r"{nom}","")
+
+        if str(prenom) != "nan" :
+            body = body.replace(r"{prenom}", " " + str(prenom).replace(" ",""))
+        else:
+            body = body.replace(r"{prenom}","")
+
         return body
 
 def import_csv(file_name : str, sep : str = ',') -> pd.DataFrame:
@@ -56,26 +70,43 @@ def send_emails(
 destinataires_path : str,
 attachment_path : str,
 body_path : str,
-subject : str
+subject : str,
+sep : str = ','
 ) -> None:
     """Send emails to the recipients in the csv file"""
-    dests = import_csv(destinataires_path, sep = ';')
-    for i in tqdm(range(len(dests))):
-        msg = MIMEMultipart()
-        body = write_body(body_path,dests[CIVILITE][i],dests[NOM][i],dests[PRENOM][i])
-        msg["from"] = USERNAME
-        msg["to"] = dests[MAIL][i]
-        msg["subject"] = subject
-        msg.attach(MIMEText(body, "html"))
-        msg.attach(import_attachment(attachment_path))
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(SERVER_NAME, SERVER_PORT, context=context) as server:
-            server.login(USERNAME, PASSWORD)
-            server.sendmail(USERNAME, dests[MAIL][i], msg.as_string())
+    dests = import_csv(destinataires_path, sep = sep)
+    context = ssl.create_default_context()
+    server = smtplib.SMTP_SSL(SERVER_NAME, SERVER_PORT, context=context)
+    server.login(USERNAME, PASSWORD)
+    for i in tqdm(range(len(dests))):     
+        if str(dests[MAIL][i]) != "nan":
+            msg = MIMEMultipart()
+            body = write_body(body_path,dests[CIVILITE][i],dests[NOM][i],dests[PRENOM][i])
+            msg["from"] = USERNAME
+            dest_email = str(dests[MAIL][i]).replace(" ","")
+            # dest to lower case
+            dest_email = dest_email.lower()
+            dest_email = unidecode(dest_email)
+            msg["to"] = dest_email
+            msg["subject"] = subject
+            msg.attach(MIMEText(body, "html"))
+            msg.attach(import_attachment(attachment_path))
+            # print("no mail sent")
+            try :
+                server.sendmail(USERNAME, dest_email, msg.as_string())
+            except smtplib.SMTPRecipientsRefused:
+                print("Refused")
+            except smtplib.SMTPSenderRefused:
+                print("Sender refused")
+                time.sleep(140)
+                server = smtplib.SMTP_SSL(SERVER_NAME, SERVER_PORT, context=context)
+                server.login(USERNAME, PASSWORD)
+
+
+
 
 
 if __name__ == '__main__':
-    subject = "Test publipostage V1"
-    print(PASSWORD,USERNAME,ATTACHMENT_PATH,RECIPIENTS_PATH)
-    print(type(PASSWORD))
+    subject = "Candidat pour reprise B.SEGUI"
+
     send_emails(RECIPIENTS_PATH,ATTACHMENT_PATH, body_path= "body.html",subject = subject)
